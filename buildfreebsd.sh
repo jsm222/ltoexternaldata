af50ee6e928404940445e5b7457ee6484e4e3d23
Jesper Schmitz Mouridsen <jesper@schmitz.computer>
2026-04-16 16:16:41 +0200

    add -flto to mainfltoworks

--- buildfreebsd.sh
+++ buildfreebsd.sh
@@ -3,7 +3,7 @@ rm ./main ./shlib.so ./mainltofails ./mainltoworks ./mainnolto
 set -e
 /usr/local/llvm20/bin/clang++ -std=c++17 -O2 -shared -fPIC -Wl,-Bsymbolic-functions shlib.cpp  -fno-direct-access-external-data -o shlib.so
 /usr/local/llvm20/bin/clang++ -std=c++17 -O2 -fPIC -flto -O2 -DBUG=1 -fno-direct-access-external-data main.cpp ./shlib.so -Wl,-rpath=. -o mainltofails
-/usr/local/llvm20/bin/clang++ -std=c++17 -O2 -fPIC -O2 -fno-direct-access-external-data main.cpp ./shlib.so -Wl,-rpath=. -o mainltoworks
+/usr/local/llvm20/bin/clang++ -std=c++17 -O2 -fPIC -flto -O2 -fno-direct-access-external-data main.cpp ./shlib.so -Wl,-rpath=. -o mainltoworks
 /usr/local/llvm20/bin/clang++ -std=c++17 -O2 -fPIC -O2 -DBUG=1 -fno-direct-access-external-data main.cpp ./shlib.so -Wl,-rpath=. -o mainnolto
 
 
